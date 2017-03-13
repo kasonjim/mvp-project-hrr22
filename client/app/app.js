91b7b4dd7cd2bb47d729b9d2ed99713636fa5a31
Jason Kim <kasonjim@gmail.com>
2017-03-13 14:27:50 -0700

    create base layout for router in app.js and add assets

---
+++ client/app/app.js
@@ -0,0 +1,19 @@
+angular.modules('sendhalp', [
+
+])
+.config(function($routeProvider, $locationProvider) {
+  $routeProvider
+    .when()
+    .when()
+    .when()
+    .otherwise();
+  //templateUrl
+  //controller
+  //redirectTo
+
+  // browse
+  // saved
+  // submit
+  //404
+  $locationProvider.hashPrefix('');
+});
--- server/server-config.js
+++ server/server-config.js
@@ -2,10 +2,13 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var handler = require('./request-handler.js');
 
+// Initialize express app
 var app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+// Ensure we are setting the directory that has our actual website
 app.use(express.static(__dirname + "/../client"));
 
 // Getting and Posting Food/Beverage entries
