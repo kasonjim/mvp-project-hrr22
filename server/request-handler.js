c2dbc87fd0ea2e61a3997bedd67bf2edca45672c
Jason Kim <kasonjim@gmail.com>
2017-03-13 12:25:12 -0700

    add basic routing template to server for get/post requests

---
+++ server/request-handler.js
@@ -0,0 +1,19 @@
+exports.getEntries = function(req, res) {
+
+};
+
+exports.postEntry = function(req, res) {
+
+};
+
+exports.getPhrases = function(req, res) {
+
+};
+
+exports.redirect = function(req, res) {
+
+};
+
+exports.reject = function(req, res) {
+
+};
--- server/server-config.js
+++ server/server-config.js
@@ -1,7 +1,22 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var handler = require('./request-handler.js');
 
 var app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 app.use(express.static(__dirname + "/../client"));
+
+// Getting and Posting Food/Beverage entries
+app.get('/api/entries', handler.getEntries);
+app.post('/api/entries', handler.postEntry);
+
+// Get phrases from the library
+app.get('/api/phrases', handler.getPhrases);
+
+// Handle other routes not defined
+app.get('/*', handler.redirect);
+app.post('/*', handler.reject);
 
 module.exports = app;
