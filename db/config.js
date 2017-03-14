835a51ea5cd81a06344b60cdc0ed85efe7484809
Jason Kim <kasonjim@gmail.com>
2017-03-14 14:00:29 -0700

    switch db to point to heroku server

--- db/config.js
+++ db/config.js
@@ -1,8 +1,8 @@
 var mongoose = require('mongoose');
 var defaults = require('./defaults'); // this will run the functions inside default.js on init
 
-//mongoose.connect('mongodb://kasonjim:kasonjim@ds131480.mlab.com:31480/heroku_p4pk8lvs');
-mongoose.connect('mongodb://localhost/sendhalpDB');
+mongoose.connect('mongodb://kasonjim:kasonjim@ds131480.mlab.com:31480/heroku_p4pk8lvs');
+//mongoose.connect('mongodb://localhost/sendhalpDB');
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
