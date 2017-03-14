0cf1f285415ce726f74c9088210ae30a97ce2000
Jason Kim <kasonjim@gmail.com>
2017-03-13 18:48:57 -0700

    moved all storage to factories instead of storing in browseController

--- client/app/browse/browseController.js
+++ client/app/browse/browseController.js
@@ -2,7 +2,7 @@ angular.module('sendhalp.browse', [
   'sendhalp.factories'
 ])
 
-.controller('BrowseController', function($scope, Saved) {
+.controller('BrowseController', function($scope, Saved, Entries, Phrases) {
   var foodString = 'food';
   var drinkString = 'drink';
 
@@ -17,39 +17,12 @@ angular.module('sendhalp.browse', [
     'drink': 'Them 22\'s givin me the run-around.'
   };
 
-  $scope.phrases = {
-    // LATER TO BE POPULATED FROM "GET" REQUEST
-    'food': [
-      { id: 1, category: 'food', text: 'Feast me some of them...' },
-      { id: 2, category: 'food', text: 'Shovel me a truck full of...' },
-      { id: 3, category: 'food', text: 'Grind it up and snort me a kilo of...' },
-      { id: 4, category: 'food', text: 'Pound me with a tray of...' }
-    ],
-    'drink': [
-      { id: 5, category: 'drink', text: 'Drink me some good...' },
-      { id: 6, category: 'drink', text: 'Drown me in some...' },
-      { id: 7, category: 'drink', text: 'Pour me a bucket of...' },
-      { id: 8, category: 'drink', text: 'Let me bathe in a river of...' },
-      { id: 9, category: 'drink', text: 'Inject me with a large dose of...' }
-    ]
-  };
+  $scope.phrases = Phrases.phrases;
 
-  $scope.entries = {
-    // LATER TO BE POPULATED FROM "GET" REQUEST
-    'food': [
-      { id: 1, category: 'food', title: 'test FOOD entry 1', url: 'www.1.com', description: 'asdf', submittedBy: 'Ife', thumbnail: 'src' },
-      { id: 2, category: 'food', title: 'test FOOD entry 2', url: 'www.2.com', description: 'q2r3', submittedBy: 'Tre', thumbnail: 'src' },
-      { id: 3, category: 'food', title: 'test FOOD entry 3', url: 'www.3.com', description: 'fiew', submittedBy: 'Robin', thumbnail: 'src' }
-    ],
-    'drink': [
-      { id: 4, category: 'drink', title: 'test DRINK entry 1', url: 'www.4.com', description: 'oepw', submittedBy: 'Tre', thumbnail: 'src' },
-      { id: 5, category: 'drink', title: 'test DRINK entry 2', url: 'www.5.com', description: '49zz', submittedBy: 'Cody', thumbnail: 'src' },
-      { id: 6, category: 'drink', title: 'text DRINK entry 3', url: 'www.6.com', description: '9328', submittedBy: 'Luke', thumbnail: 'src' }
-    ]
-  };
+  $scope.entries = Entries.entries;
 
   $scope.switchButtonText = {
-    'food': 'I ain\'t hungry, gimme a drink',   // If on food page, text will say to switch to drink page
+    'food': 'I ain\'t hungry, gimme a drink',
     'drink': 'Can\'t drink on an empty stomach'
   };
 
@@ -57,13 +30,6 @@ angular.module('sendhalp.browse', [
     var index = Math.floor(Math.random() * $scope.phrases[$scope.currCategory].length);
     $scope.currPhrase = $scope.phrases[$scope.currCategory][index]["text"];
   };
-
-
-  // THIS IS FOR THE 'GET' REQUEST FUNCTION, TO KNOW WHAT DATA TO EXPECT AND TO SORT BY 'CATEGORY'
-    // need to autopopulate the $scope.phrases and $scope.entries
-    // Entries: { id, category, title, url, description, submittedBy, thumbnail source  } - included: createdAt/modifiedAt
-    // Phrases: { id, category, text } - included: createdAt/modifiedAt
-  // finish popup stuff below in addToList
 
   $scope.addToList = function() {
     var success = Saved.addEntry($scope.currEntry["id"]);
--- client/app/factories.js
+++ client/app/factories.js
@@ -25,10 +25,65 @@ angular.module('sendhalp.factories', [])
   };
 })
 
-.factory('Entries', function($http) {
-  // functions to get and post entries
+.factory('Phrases', function($http) {
+  // THIS IS FOR THE 'GET' REQUEST FUNCTION, TO KNOW WHAT DATA TO EXPECT AND TO SORT BY 'CATEGORY'
+  // need to autopopulate the $scope.phrases
+  // Phrases: { id, category, text } - included: createdAt/modifiedAt
+
+  var phrases = {
+    // LATER TO BE POPULATED FROM "GET" REQUEST
+    'food': [
+      { id: 1, category: 'food', text: 'Feast me some of them...' },
+      { id: 2, category: 'food', text: 'Shovel me a truck full of...' },
+      { id: 3, category: 'food', text: 'Grind it up and snort me a kilo of...' },
+      { id: 4, category: 'food', text: 'Pound me with a tray of...' }
+    ],
+    'drink': [
+      { id: 5, category: 'drink', text: 'Drink me some good...' },
+      { id: 6, category: 'drink', text: 'Drown me in some...' },
+      { id: 7, category: 'drink', text: 'Pour me a bucket of...' },
+      { id: 8, category: 'drink', text: 'Let me bathe in a river of...' },
+      { id: 9, category: 'drink', text: 'Inject me with a large dose of...' }
+    ]
+  };
+
+
+
+  // function to get phrases from server
+
+
+
+  return {
+    phrases: phrases
+  };
 })
 
-.factory('Phrases', function($http) {
-  // function to get phrases from server
+.factory('Entries', function($http) {
+  // THIS IS FOR THE 'GET' REQUEST FUNCTION, TO KNOW WHAT DATA TO EXPECT AND TO SORT BY 'CATEGORY'
+    // need to autopopulate the $scope.entries
+    // Entries: { id, category, title, url, description, submittedBy, thumbnail source  } - included: createdAt/modifiedAt
+
+  var entries = {
+    // LATER TO BE POPULATED FROM "GET" REQUEST
+    'food': [
+      { id: 1, category: 'food', title: 'test FOOD entry 1', url: 'www.1.com', description: 'asdf', submittedBy: 'Ife', thumbnail: 'src' },
+      { id: 2, category: 'food', title: 'test FOOD entry 2', url: 'www.2.com', description: 'q2r3', submittedBy: 'Tre', thumbnail: 'src' },
+      { id: 3, category: 'food', title: 'test FOOD entry 3', url: 'www.3.com', description: 'fiew', submittedBy: 'Robin', thumbnail: 'src' }
+    ],
+    'drink': [
+      { id: 4, category: 'drink', title: 'test DRINK entry 1', url: 'www.4.com', description: 'oepw', submittedBy: 'Tre', thumbnail: 'src' },
+      { id: 5, category: 'drink', title: 'test DRINK entry 2', url: 'www.5.com', description: '49zz', submittedBy: 'Cody', thumbnail: 'src' },
+      { id: 6, category: 'drink', title: 'text DRINK entry 3', url: 'www.6.com', description: '9328', submittedBy: 'Luke', thumbnail: 'src' }
+    ]
+  };
+
+
+
+  // functions to get and post entries
+
+
+
+  return {
+    entries: entries
+  };
 });
