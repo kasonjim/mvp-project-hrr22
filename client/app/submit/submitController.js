13f053c8e4a109b56809df77700142e480b5558c
Jason Kim <kasonjim@gmail.com>
2017-03-13 20:24:40 -0700

    complete basic layout for submit html page

--- client/app/submit/submitController.js
+++ client/app/submit/submitController.js
@@ -1,4 +1,11 @@
 angular.module('sendhalp.submit', [])
 .controller('SubmitController', function($scope) {
 
+  $scope.submitNewEntry = function() {
+    console.log('submission', $scope.submittedBy);
+    console.log('submission', $scope.category);
+    console.log('submission', $scope.title);
+    console.log('submission', $scope.description);
+    console.log('submission', $scope.url);
+  };
 });
